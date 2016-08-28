ae72457134cc65742d36370c2a8eaf2c4685213c
Beto Esquivel <ppbeto94@gmail.com>
2016-08-28 12:31:34 +0100

    Works with db streams and then generate

--- handler.js
+++ handler.js
@@ -1,8 +1,34 @@
 'use strict';
 
-// Your first function handler
-module.exports.generate = (event, context, cb) => cb(null,
-  { message: 'Go Serverless v1.0! Your function executed successfully!', event }
-);
+
+var manager = require('./lib/manager.js');
+
+module.exports.generate = (event, context, cb) => {
+
+  const dynamoRecords = manager.parseDynamoEvents(event);
+  if (dynamoRecords) {
+    console.log(`Got ${event.Records.length} dynamo events`);
+    console.log(`Parsed ${dynamoRecords.length} records`);
+
+    const insertRecords = manager.newRecordEvents(dynamoRecords);
+    console.log(`Filtered and got ${insertRecords.length} filtered records`);
+
+    console.log(JSON.stringify(insertRecords));
+    console.log(`Calling lambda with ${JSON.stringify(insertRecords[0])}`);
+    manager.lambdaCreateLSystem(insertRecords[0]).then((msg) =>{
+
+      console.log(`Response ${JSON.stringify(msg)}`);
+      cb(null,`Response from lambda: ${msg}`);
+    });
+  } else {
+
+    cb(null, {
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      event
+    });
+
+  }
+
+};
 
 // You can add more handlers here, and reference them in serverless.yml
